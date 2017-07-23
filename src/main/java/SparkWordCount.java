77563b100a17fd1bd4d52f8484ce46cd2bad95c6
Joseungwan <wh7923@naver.com>
2017-07-23 17:41:47 +0900

    java1.7_spark2.2 제작

--- src/main/java/SparkWordCount.java
+++ src/main/java/SparkWordCount.java
@@ -1,3 +1,4 @@
+import org.apache.commons.collections.iterators.ArrayListIterator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -26,7 +27,12 @@ public class SparkWordCount {
 
         //countByKey()를 사용하여 빠르게 단어수 얻기
         Map<String, Long> fastCount = logData
-                .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+                .flatMap(new FlatMapFunction<String, String>() {
+	                @Override
+	                public Iterator<String> call(String s) throws Exception {
+		                return new ArrayListIterator(s.split(" "));
+	                }
+                })
                 .countByValue();
 
         for (String key : fastCount.keySet()) {
@@ -37,9 +43,24 @@ public class SparkWordCount {
         //new Tuple2<>(word, 1)은 듀플 자료형으로 (단어, 1)이런식으로 저장해라는 뜻이다.
         //java에는 Tuple자료형이 없기 때문에 spark에서 제공하는 Tuple2()함수를 사용한다.
         JavaPairRDD<String, Integer> counts = logData
-                .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
-                .mapToPair(word -> new Tuple2<>(word, 1))
-                .reduceByKey((a,b) -> a + b);
+                .flatMap(new FlatMapFunction<String, String>() {
+	                @Override
+	                public Iterator<String> call(String s) throws Exception {
+		                return new ArrayListIterator(s.split(" "));
+	                }
+                })
+                .mapToPair(new PairFunction<String, String, Integer>() {
+	                @Override
+	                public Tuple2<String, Integer> call(String s) throws Exception {
+		                return new Tuple2<>(s,1);
+	                }
+                })
+                .reduceByKey(new Function2<Integer, Integer, Integer>() {
+	                @Override
+	                public Integer call(Integer i1, Integer i2) throws Exception {
+		                return i1 + i2;
+	                }
+                });
 
         //lambda식 풀어 쓰기
         JavaRDD<String> word = logData.flatMap(new FlatMapFunction<String, String>() {
